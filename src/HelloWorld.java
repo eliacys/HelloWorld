a81a2d5b907f3aa25e25ea65631c7fc602ee7b17
eliacys <eliacyneta@gmail.com>
2018-11-22 18:08:02 -0300

    Primeiro código

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+    
+    public static void main(String[] args){
+        System.out.println("Olá Galera");
+        System.out.println("Daqui a puco é 2019");
+        
+        
+        
+    }
+}
